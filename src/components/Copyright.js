a03cd3ffb142a89bf32ecdc28e7c9235dfa9a117
Siddharth Sharma <siddharth19n@gmail.com>
2021-11-29 21:41:01 +0530

    changed the copyright

--- src/components/Copyright.js
+++ src/components/Copyright.js
@@ -8,7 +8,7 @@ function Copyright() {
                 <div className="container">
                     <div className="row">
                         <div className="col-lg-12">
-                            <p className="p-small">Copyright © 2021 Patria & Co. - All rights reserved</p>
+                            <p className="p-small">Copyright © 2021 <a href="https://the-fountainhead.business.site/?m=true">TheFountainhead</a> - All rights reserved</p>
                         </div> 
                     </div> 
                 </div> 
