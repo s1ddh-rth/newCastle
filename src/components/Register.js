f6adbdf6037004b7854153bb34e0d04768698853
Siddharth Sharma <siddharth19n@gmail.com>
2021-12-16 21:09:38 +0530

    made changes to registration form

--- src/components/Register.js
+++ src/components/Register.js
@@ -4,27 +4,37 @@ import Axios from 'axios';
 
 function Register() {
     // const [register, setRegister] = useState(false);
-    const [name, setName] = useState('');
+    const [fname, setFname] = useState('');
+    const [lname, setLname] = useState('');
+    const [mname, setMname] = useState('');
     const [email, setEmail] = useState('');
-    const [phone, setPhone] = useState('');
+    const [phone, setPhone] = useState();
+    const [company, setCompany] = useState();
+
+    
 
     const handleSubmit = (e) => {
     e.preventDefault();
     const data = {
-        Name: name,
+        FirstName: fname,
+        MiddleName: mname,
+        LastName: lname,
         Email: email,
-        Phone: phone
+        Phone: phone,
+        Company: company
     };
-    if(data.Name !=='' && data.Email !=='' && data.Phone !==''){
+    if(data.FirstName !=='' && data.LasttName !=='' && data.Email !=='' && data.Phone !==''){
         Axios.post('https://sheet.best/api/sheets/58e46e80-13b8-483c-ad87-60ccde374d85',data).then((response)=>{
-            setName('');
+            console.log(response.status);
+            setFname('');
+            setMname('');
+            setLname('');
             setPhone('');
             setEmail('');
-            // setRegister(true);
+            setCompany('');
+            alert("Registered Successfully");
     });
-    };
-    
-
+    }
     }
 
     return (
@@ -58,7 +68,15 @@ function Register() {
                             <div className="form-container">
                                 <form id="registrationForm" data-toggle="validator" data-focus="false" onSubmit={handleSubmit} >
                                     <div className="form-group">
-                                        <input type="text" className="form-control-input" id="rname" name="rname" placeholder="Complete name" required onChange={(e)=>{setName(e.target.value)}} value={name} />
+                                        <input type="text" className="form-control-input" id="rfname" name="rfname" placeholder="First name" required onChange={(e)=>{setFname(e.target.value)}} value={fname} />
+                                        <div className="help-block with-errors"></div>
+                                    </div>
+                                    <div className="form-group">
+                                        <input type="text" className="form-control-input" id="rmname" name="rmname" placeholder="Middle name" onChange={(e)=>{setMname(e.target.value)}} value={mname} />
+                                        <div className="help-block with-errors"></div>
+                                    </div>
+                                    <div className="form-group">
+                                        <input type="text" className="form-control-input" id="rlname" name="rlname" placeholder="Last name" required onChange={(e)=>{setLname(e.target.value)}} value={lname} />
                                         <div className="help-block with-errors"></div>
                                     </div>
                                     <div className="form-group">
@@ -69,7 +87,10 @@ function Register() {
                                         <input type="text" className="form-control-input" id="rphone" name="rphone" placeholder="Phone number" required onChange={(e)=>{setPhone(e.target.value)}} value={phone} />
                                         <div className="help-block with-errors"></div>
                                     </div>
-                                    
+                                    <div className="form-group">
+                                        <input type="text" className="form-control-input" id="rcompany" name="rcompany" placeholder="Company (Optional)" onChange={(e)=>{setCompany(e.target.value)}} value={company} />
+                                        <div className="help-block with-errors"></div>
+                                    </div>
                                     <div className="form-group">
                                         <button type="submit" className="form-control-submit-button">REGISTER</button>
                                     </div>
